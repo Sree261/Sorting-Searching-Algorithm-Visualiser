67d3d16acd9bcfde74800d6509da8bcfb02894b3
Sreeram M <69712963+Sree261@users.noreply.github.com>
2021-07-04 23:19:24 +0530

    Delete draw_fns.h

--- Header/draw_fns.h
+++
@@ -1,8 +0,0 @@
-#ifndef DRAW_FNS_H_INCLUDED
-#define DRAW_FNS_H_INCLUDED
-#include<bits/stdc++.h>
-using namespace std;
-void draw(vector<int> &arr,int size);
-void create_window(int val);
-
-#endif // DRAW_FNS_H_INCLUDED
